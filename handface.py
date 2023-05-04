cf62dd663b86e1717c3093040f1550bcfc4ecf90
mehisanyam <126437755+mehisanyam@users.noreply.github.com>
2023-05-04 09:09:03 +0530

    Add files via upload

---
+++ handface.py
@@ -0,0 +1,51 @@
+import cv2
+import mediapipe as mp
+import time
+
+cap=cv2.VideoCapture(0)
+
+mpHands= mp.solutions.hands
+hands=mpHands.Hands()
+mpDraw = mp.solutions.drawing_utils
+
+mpDraw = mp.solutions.drawing_utils
+mpFaceMesh = mp.solutions.face_mesh
+faceMesh = mpFaceMesh.FaceMesh(max_num_faces=2)
+drawSpec = mpDraw.DrawingSpec(thickness=1, circle_radius=1)
+
+pTime=0
+cTime=0
+
+while True:
+    success, img= cap.read()
+    imgRGB=cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = hands.process(imgRGB)
+    results1 = faceMesh.process(imgRGB)
+    #print(results.multi_hand_landmarks)
+
+    if results1.multi_face_landmarks:
+        for faceLms in results1.multi_face_landmarks:
+            mpDraw.draw_landmarks(img, faceLms, mpFaceMesh.FACEMESH_CONTOURS,
+                                  drawSpec,drawSpec)
+
+    if results.multi_hand_landmarks:
+        for handLms in results.multi_hand_landmarks:
+            for id, lm in enumerate(handLms.landmark):
+                #print(id,lm)
+                h,w,c=img.shape
+                cx,cy=int(lm.x*w), int(lm.y*h)
+                print(id, cx, cy)
+                if id==4:
+                    cv2.circle(img, (cx,cy),10 ,(95,0,255),cv2.FILLED)
+            mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
+
+    cTime=time.time()
+    fps=1/(cTime-pTime)
+    pTime=cTime
+
+    cv2.putText(img,str(int(fps)),(10,70), cv2.FONT_HERSHEY_PLAIN,3,(95,10,255),3)
+
+
+
+    cv2.imshow("Image",img)
+    cv2.waitKey(1)
